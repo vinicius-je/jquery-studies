df5fddc9648ecb3b4b540b76625ba1b05867d310
Vinicius Estevam <vinicius.estevam99@gmail.com>
2021-05-04 12:11:32 -0300

    Update: modifying elements

--- aula08-modifying/script.js
+++ aula08-modifying/script.js
@@ -17,3 +17,32 @@ $("li").attr("name",(i, oValue) => {
 
 $("input").attr("maxlength", 5).attr("type", "password");
 
+// Selecionando elemento por atributo
+// console.log($("[key = 2]").remove());
+
+// Empty -> remove todo os filhos
+// lista.empty();
+
+// Inserir elementos
+// let itens = lista.html();
+// lista.html(itens + "<li> teste </li>");
+
+// Inserir elementos com append => adicionou um elementou no final
+// lista.append("<li> novo teste </li>")
+
+// Inserir elemento no início
+// lista.prepend("<li> novo teste </li>")
+
+// Inseri após o fechamento da lista 
+// lista.after("<li> novo teste </li>")
+
+// Inseri antes o abrimento da lista 
+// lista.before("<li> novo teste 2 </li>")
+
+let rec = $(".rect");
+
+rec.append("append");
+rec.prepend("prepend");
+rec.after("after");
+rec.before("before");
+
